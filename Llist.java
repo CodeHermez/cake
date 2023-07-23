bb7d53bf0ec5b87e715356c05ccbc5fccfe971d6
Hamza <u22575172@tuks.co.za>
2023-07-23 20:17:13 +0200

    Added contains,insert and print

--- Llist.java
+++ Llist.java
@@ -1,6 +1,8 @@
 public class Llist{
 
-    Llist(){}
+    Llist(){
+        root = null;
+    }
     public void rm_message(int a)
     {
         switch (a) {
@@ -90,7 +92,49 @@ public class Llist{
         return  word;
     }
 
+    void insert(int num) {
+        if (root == null) {
+            root = new Node();
+            root.data = num;
+        } else {
+            Node nodePtr = root;
+            while (nodePtr.next != null) {
+                nodePtr = nodePtr.next;
+            }
+            nodePtr.next = new Node();
+            nodePtr.next.data = num;
+        }
+    }
 
+    Boolean contains(int num) {
+        if (root == null) {
+            return false;
+        }
+
+        Node nodePtr = root;
+        while (nodePtr != null) {
+            if (nodePtr.data == num) {
+                return true;
+            } else{
+                nodePtr = nodePtr.next;
+            }
+        }
+
+        return false;
+    }
+
+    void print(){
+        if (root == null) {
+            return;
+        }
+
+        Node nPtr = root;
+        while (nPtr != null) {
+            System.out.print(nPtr.data + " ");
+
+        }
+
+    }
 
 
     Node root;
